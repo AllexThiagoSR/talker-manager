e23c12aa21e2217a431c9bb4999867b930e7a7cb
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-17 17:12:09 -0300

    Requisito 3: Implementa a rota /login que retorna um token de 16 caracteres aleatórios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
 const talkerRouter = require('./talker.routes');
+const generateToken = require('../utils/generateToken');
 
 const indexRouter = Router();
 
 indexRouter.use('/talker', talkerRouter);
+
+indexRouter.post('/login', (_, res) => {
+  const token = generateToken();
+  console.log(token.length);
+  return res.status(200).json({ token });
+});
 
 module.exports = indexRouter;
